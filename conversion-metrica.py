cbc00fb8ab5282c69f9561b87f9f2af731543072
JeroFerrer93 <jeronimoferrer4@gmail.com>
2024-12-11 21:43:02 -0300

    Programa para convertir de cms a pulgadas

---
+++ conversion-metrica.py
@@ -0,0 +1,5 @@
+cms = float(input("Por favor ingresar las medidas de la pieza del mueble en centimetros: "))
+
+medida_en_pulgadas = cms / 2.54
+
+print("Las medidas en pulgadas de la pieza ingresada son: ", medida_en_pulgadas)
